2129daf310bfa71304424c78ca7c4aeee2a6f232
chinathanna001 <90709670+chinathanna001@users.noreply.github.com>
2023-01-17 14:52:16 +0530

    Initial commit

---
+++ Auth/firebaseauth.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAF0oB4pUeVsRUxxVFEK9OG76J5oPJ2KtY",
+    authDomain: "j-a-r-v-i-z.firebaseapp.com",
+    databaseURL: "https://j-a-r-v-i-z-default-rtdb.firebaseio.com",
+    projectId: "j-a-r-v-i-z",
+    storageBucket: "j-a-r-v-i-z.appspot.co",
+    messagingSenderId: "227647255325",
+    appId: "1:227647255325:web:12259d56206d8b552a67f4"
+  };
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
+  var auth = firebase.auth();
