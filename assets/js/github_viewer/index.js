049c6c27a75e36dd2e7fb9a51f1b2b6b7bed72b5
Sharon Joseph <90709670+josephsharon07@users.noreply.github.com>
2023-11-27 09:29:54 +0530

    Update index.js

--- assets/js/github_viewer/index.js
+++ assets/js/github_viewer/index.js
@@ -83,7 +83,7 @@ size = item.size < 1048576 ? (((item.size)/(1024)).toFixed(2) + " KB"):(((item.s
 
 url = `/file_viewer/program/index.html?target=${item.html_url}&style=atom-one-dark&type=code&showBorder=on&showLineNumbers=on&showFileMeta=on&showCopy=on&fetchFromJsDelivr=on&url=${item.url}`
 }
-else if (item.name.endsWith('.txt'))
+else if (item.name.endsWith('.txt') || item.name.endsWith('.url'))
 {
   icon = `<svg class="bi bi-file-text-fill" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" style="width: 36;height: 36;">
   <path d="M12 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zM5 4h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1zm0 2h3a.5.5 0 0 1 0 1H5a.5.5 0 0 1 0-1z"></path>
